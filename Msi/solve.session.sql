2e9aec0c75fcb313b59a3b0ac758463b60714d37
Marcos Vinícius <134982216+mvrm-dev@users.noreply.github.com>
2025-04-28 12:15:06 -0300

    att

--- Msi/solve.session.sql
+++
@@ -1,8 +0,0 @@
---@block
-SELECT nome, funcao, nomeObra, hora_do_dia FROM funcionarios INNER JOIN obras ON obras.ref = funcionarios.id
-
---@block
-SELECT * FROM funcionarios
-
---@block
-SELECT * FROM obras
