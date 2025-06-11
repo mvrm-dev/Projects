589d1d1d1efdd32ccc9177301cfe8b2de29a0552
marcos vinícius <marcosmantoani90@gmail.com>
2025-06-11 10:25:11 -0300

    adicionando novo projeto

---
+++ Node/server.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const {createServer} = require('http')
+
+const data = JSON.stringify([
+    {id: 'A1', name: 'Vacuum Cleaner', rrp: '99.99', info: 'The worst vacuum in the world'},
+    {id: 'A2', name: 'Leaf Blower', rrp: '303.33', info: 'This produt WILL blow your socks off'},
+    {id: 'B1', name: 'Chocolate Bar', rrp: '22.43', info: 'Delicious overpriced chocolate'}
+])
+
+
+
