5be9708ab4a4b678ee5c5e7a35eade5706a81db4
marcos vinícius <marcosmantoani90@gmail.com>
2025-06-23 23:33:02 -0300

    Adicionando funcionalidade: adicionar produto

--- Node/mock-srv/routes/electronics/index.js
+++ Node/mock-srv/routes/electronics/index.js
@@ -1,10 +1,18 @@
 'use strict'
 
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return [
+const data = [
       {id: 'A1', name: 'Vacuum Cleaner', rrp: '99.99', info: 'The worst vacuum in the world.'},
       {id: 'A2', name: 'Leaf Blower', rrp: '303.33', info: 'This product will blow your socks off.'}
     ]
+
+module.exports = async function (fastify, opts) {
+  fastify.get('/', async function (request, reply) {
+    return data
+  })
+  
+  fastify.post('/', async function(request, reply) {
+
+    fastify.mockDataInsert(request, opts.prefix.slice(1), data)
+    return data
   })
 }
